b5ae59c4255eec553f95493865bee91f1403e93b
Marcio Silveira <mhsilveira.br@gmail.com>
2020-06-26 11:11:13 -0300

    refactor

--- naive_bayes.py
+++ naive_bayes.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
 from sklearn.metrics import classification_report
 
 data = pd.read_csv('impeachment_br.csv').dropna()
@@ -27,16 +27,20 @@ X_train, X_test, y_train, y_test = train_test_split(features,
                                                     test_size=0.3,
                                                     random_state=88)
 
-naive_bayes = MultinomialNB()
+multinomial_nb = MultinomialNB().fit(X_train, y_train)
+# gaussian_nb = GaussianNB().fit(X_train, y_train)
+bernoulli_nb = BernoulliNB().fit(X_train, y_train)
 
-naive_bayes.fit(X_train, y_train)
+y_pred_multinomial = multinomial_nb.predict(X_test)
+# y_pred_gaussian = gaussian_nb.predict(X_test)
+y_pred_bernoulli = bernoulli_nb.predict(X_test)
 
-y_pred = naive_bayes.predict(X_test)
+print(classification_report(y_test, y_pred_multinomial))
+# print(classification_report(y_test, y_pred_gaussian))
+print(classification_report(y_test, y_pred_bernoulli))
 
-print(classification_report(y_test, y_pred))
 
-
-example = ['deus familia militares', 'democracia estado direito']
+example = ['deus familia militares', 'democracia estado direito', 'democracia deus familia']
 doc_term_matrix = vectorizer.transform(example)
 y_pred_example = naive_bayes.predict_proba(doc_term_matrix)
 print(y_pred_example)
